b5c74bf2021c413d19a703c38f1accf9b188e1de
Collins07 <collinsabaya07@gmail.com>
2023-01-31 12:12:42 +0300

    email validation

--- reforestation/reforestation/static/js/register.js
+++ reforestation/reforestation/static/js/register.js
@@ -1,9 +1,45 @@
 const usernameField = document.querySelector('#usernameField');
-const feedbackArea = document.querySelector('.invalid_feedback')
+const feedbackArea = document.querySelector('.invalid_feedback');
+const emailField = document.querySelector('#emailField');
+const emailfeedbackArea = document.querySelector('.emailfeedbackArea');
+const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+
+
+emailField.addEventListener('keyup', (e)=>{
+
+    const emailVal = e.target.value;
+
+    emailField.classList.remove("is-invalid");
+    emailfeedbackArea.style.display = "none";
+
+    if(emailVal.length > 0){
+    fetch("/authentication/validate-email",{
+        body: JSON.stringify({email: emailVal}),
+        method:"POST",
+
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log('data', data)
+        if(data.email_error){
+            emailField.classList.add("is-invalid");
+            emailfeedbackArea.style.display = "block";
+            emailfeedbackArea.innerHTML=(data.email_error);
+        }
+    });
+  } 
+
+})
+
+
+
+
+
 usernameField.addEventListener("keyup", (e) => {
     console.log('77777', 77777);
 
     const usernameVal = e.target.value;
+    usernameSuccessOutput.textContent= 'Checking ${usernameVal}'
 
     usernameField.classList.remove("is-invalid");
     feedbackArea.style.display = "none";
@@ -17,6 +53,7 @@ usernameField.addEventListener("keyup", (e) => {
     .then(res=>res.json())
     .then(data=>{
         console.log('data', data)
+        usernameSuccessOutput.style.display = "none";
         if(data.username_error){
             usernameField.classList.add("is-invalid");
             feedbackArea.style.display = "block";
