83841878ab163ca109b5aeac35d2020a0b36c86b
Collins07 <collinsabaya07@gmail.com>
2023-01-31 11:16:10 +0300

    username validation

--- reforestation/authentication/views.py
+++ reforestation/authentication/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 import json
 
 
-
 # Create your views here.
 class UsernameValidationView(View):
     def post(self, request):
@@ -13,7 +12,11 @@ class UsernameValidationView(View):
         username = data['username']
 
         if not str(username).isalnum():
-            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'})
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status=400)
+        
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username is already taken, try another one'}, status=409)
+        
         return JsonResponse({'username_valid': True})
 
 
