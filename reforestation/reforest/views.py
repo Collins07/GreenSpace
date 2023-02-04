2fd7c4fbb8f417208e57544d03a6238f08a9e38a
Collins07 <collinsabaya07@gmail.com>
2023-02-04 20:13:53 +0300

    persist data

--- reforestation/reforest/views.py
+++ reforestation/reforest/views.py
@@ -1,21 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Category, Reforest
+from django.contrib import messages
 
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'reforest/index.html')
+    reforest = Reforest.objects.all()
+    context = {
+        'reforest': reforest,
+
+    }
+    return render(request, 'reforest/index.html', context)
 
 
 
 def add_trees(request):
     categories = Category.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'values': request.POST,
     }
-    return render(request, 'reforest/add_trees.html', context)
+    
+    if request.method == 'POST':
+        trees_planted = request.POST['trees_planted']
+
+        if not trees_planted:
+            messages.error(request,'Number of trees planted required !!!')
+            return render(request, 'reforest/add_trees.html', context)
+        description = request.POST['description']
+        date = request.POST['date']
+        category = request.POST['category']
+        
+    if request.method == 'POST':
+        description = request.POST['description']
+
+        if not description:
+            messages.error(request,' The name of your group is required !!!')
+            return render(request, 'reforest/add_trees.html', context)
+        
+        Reforest.objects.create(owner=request.user, trees_planted=trees_planted, description=description, date=date, category=category)
+        messages.success(request, 'Data saved successfully')
+
+        return redirect('index')
+
+    return render(request, 'reforest/add_trees.html', context)    
 
 
 
