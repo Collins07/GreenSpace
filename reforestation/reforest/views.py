c1f217495c332c5c07cd8c675f331fbe6f11c264
Collins07 <collinsabaya07@gmail.com>
2023-02-04 14:14:04 +0300

    render categories in templates

--- reforestation/reforest/views.py
+++ reforestation/reforest/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Reforest
 
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'reforest/index.html')
 
+
+
 def add_trees(request):
-    return render(request, 'reforest/add_trees.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'reforest/add_trees.html', context)
+
+
 
 def home(request):
     return render(request, 'reforest/home.html')
