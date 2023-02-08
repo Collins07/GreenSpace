677c559f2ee91ec2ec3a4d8c06d4beaa2e76c32c
Collins07 <collinsabaya07@gmail.com>
2023-02-08 21:29:58 +0300

    search implementation

--- reforestation/reforestation/static/js/search.js
+++ reforestation/reforestation/static/js/search.js
@@ -1,10 +1,20 @@
 const searchField = document.querySelector("#searchField");
 
+const appTable = document.querySelector(".app-table");
+
+const tbody = document.querySelector(".table-body")
+
+const paginationTable = document.querySelector("#pagination");
+
+const tableOutput = document.querySelector(".table-output");
+tableOutput.style.display = "none"
+
 
 searchField.addEventListener("keyup", (e) =>{
   const searchValue = e.target.value;
 
   if(searchValue.trim().length>0){
+    paginationTable.style.display = "none"
    
 
     fetch("/search-reforest",{
@@ -14,8 +24,38 @@ searchField.addEventListener("keyup", (e) =>{
     })
     .then(res=>res.json())
     .then(data=>{
-        console.log("data", data)
+        console.log("data", data);
+
+        appTable.style.display = "none"
+
+        tableOutput.style.display = "block"
+
+        if (data.length===0){
+            tableOutput.innerHTML = 'No results found !!'
+        }
+        else{
+            data.forEach((item) => {
+
+                tbody.innerHTML =  `
+                
+                <tr> 
+                <td>$(item.trees_planted)</td>  
+                <td>$(item.trees_description)</td>  
+                <td>$(item.category)</td> 
+                <td>$(item.date)</td> 
+                </tr> 
+                
+                `;
+
+        });
+
+    }   
         
     });
+  }
+  else{
+    appTable.style.display = "block"
+    paginationTable.style.display = "block"
+    tableOutput.style.display = "none"
   }
 });
